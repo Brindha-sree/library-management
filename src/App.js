97d90d8ce770f4ef2ce053defb51a0e3820cd002
Brindha-sree <srikannan547@gmail.com>
2023-08-28 15:26:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import  Home  from './components/Home';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+import EditBook from './components/EditBook';
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<BookList />} />
+        <Route path='/addbook' element={<AddBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import imge from './img.jpg'
+import Box from '@mui/material/Box';
+
+const Home = () => {
+  return (
+    <div>
+          <Box height={100} />
+    <img src={imge} alt="" />
+    </div>
+  )
+}
+
+export default Home
